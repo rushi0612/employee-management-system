c22dd4fab4f15279b6bb5f4f171562c35650fb82
Rushikesh <rvp6123@gmail.com>
2026-06-25 14:53:09 +0530

    Setup enterprise project architecture

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import App from './App.vue'
-import router from './router'
+import "./assets/css/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
